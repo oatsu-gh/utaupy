65804865dab2bc81374bfa65918b2f3e31cf8715
oatsu-gh <panchi.psp@gmail.com>
2020-08-15 18:27:20 +0900

    register testpypi
    PyPIにアップロードできるようにはなった。

--- setup.py
+++ setup.py
@@ -2,25 +2,33 @@
 # coding: utf-8
 # Copyright (c) oatsu
 """
-PyPiからインストールするときに使うやつ
+Python script for PyPI registation
 """
-
 from setuptools import setup
 
-version = '1.6.0'
+version = '1.6.0.0'
 
-with open("README.md") as f:
-    long_description = f.read()
+try:
+    with open("README.md", mode='r') as f:
+        long_description = f.read()
+except UnicodeDecodeError:
+    with open("README.md", mode='r', encoding='utf-8') as f:
+        long_description = f.read()
 
 setup(
     name='utaupy',
     version=version,
     description='Python3 module for UTAU and singing-voice-database',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='oatsu',
     author_email='panchi.psp@gmail.com',
-    url='https://github.com/oatsu-gh/utaupy/projects',
+    maintainer='oatsu',
+    maintainer_email='panchi.psp@gmail.com',
+    url='https://github.com/oatsu-gh/utaupy',
     install_requires=['pprint'],
-    classifiiers=[
+    classifiers=[
         'Programming Language :: Python :: 3'
-    ]
+    ],
+    keywords=['UTAU']
 )
