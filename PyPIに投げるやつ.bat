6d6336293cb5e0cadcb38dfe7d71dc05dc091928
oatsu <panchi.psp@gmail.com>
2020-08-20 03:55:25 +0900

    release v1.6.3
    PyPIでのバージョン上げるの忘れてた

--- PyPIに投げるやつ.bat
+++ PyPIに投げるやつ.bat
@@ -1,5 +1,5 @@
 rd /s /q dist build utaupy.egg-info
 python setup.py sdist bdist_wheel
 twine check dist/*
-twine upload --repository testpypi dist/* --verbose
-REM twine upload --repository pypi dist/* --verbose
+REM twine upload --repository testpypi dist/* --verbose
+twine upload --repository pypi dist/* --verbose
