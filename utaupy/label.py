0f89137793f77d3804ff162ea8036819f4bd3979
oatsu <panchi.psp@gmail.com>
2020-09-12 20:34:23 +0900

    Update label.py
    ① 関数の引数の kiritan を変更し、time_unit にして、単位を入力させるように変更。s, 100ns, subus などの表記に対応。
    ② Label クラスを list の子クラスにした。

--- utaupy/label.py
+++ utaupy/label.py
@@ -10,11 +10,10 @@ def main():
     print('labファイル読み取り動作テストをします。')
     path_lab = input('path_lab: ')
     label = load(path_lab)
-    for phoneme in label.values:
-        print(phoneme.values)
+    print(label)
 
 
-def load_as_plainlist(path, mode='r', encoding='utf-8', kiritan=False):
+def load_as_plainlist(path, mode='r', encoding='utf-8', time_unit='100ns'):
     """
     labファイルを ふつうの2次元リストとして読み取る。
     旧バージョンの utaupy.label.load() に近い動作をする。
@@ -27,16 +26,19 @@ def load_as_plainlist(path, mode='r', encoding='utf-8', kiritan=False):
         del lines[-1]
 
     # リストにする [[開始時刻, 終了時刻, 発音], [], ...]
-    if kiritan:
+    if time_unit in ('s', 'sec', 'second'):
         # きりたんDBのモノラベル形式の場合、時刻が 0.0000000[s] なのでfloatを経由する。
         l = [[int(10000000 * float(v[0])), int(10000000 * float(v[1])), v[2]] for v in lines]
-    else:
+    elif time_unit in ('100ns', 'subus', 'subμs'):
         # Sinsyのモノラベル形式の場合、時刻が 1234567[100ns] なのでintにする。
         l = [[int(v[0]), int(v[1]), v[2]] for v in lines]
+    else:
+        raise ValueError(
+            'function argument "time_unit" must be in ["100ns" (recommended), "s"]')
     return l
 
 
-def load(path, mode='r', encoding='utf-8', kiritan=False):
+def load(path, mode='r', encoding='utf-8', time_unit='100ns'):
     """
     labファイルを読み取って Label クラスオブジェクトにする
     時刻を整数にすることに注意
@@ -49,54 +51,39 @@ def load(path, mode='r', encoding='utf-8', kiritan=False):
         del lines[-1]
 
     # リストにする [[開始時刻, 終了時刻, 発音], [], ...]
-    if kiritan:
-        phonemes = []
+    if time_unit in ('s', 'sec', 'second'):
+        label = Label()
         for v in lines:
-            phoneme = Phoneme()
-            phoneme.start = int(10000000 * float(v[0]))
-            phoneme.end = int(10000000 * float(v[1]))
-            phoneme.symbol = v[2]
-            phonemes.append(phoneme)
+            monolabel = Phoneme()
+            monolabel.start = int(10000000 * float(v[0]))
+            monolabel.end = int(10000000 * float(v[1]))
+            monolabel.symbol = v[2]
+            label.append(monolabel)
 
     # Sinsyのモノラベル形式の場合、時刻が 1234567[100ns] なのでintにする。
-    else:
-        phonemes = []
+    elif time_unit in ('100ns', 'subus', 'subμs'):
+        label = Label()
         for v in lines:
-            phoneme = Phoneme()
-            phoneme.start = int(v[0])
-            phoneme.end = int(v[1])
-            phoneme.symbol = v[2]
-            phonemes.append(phoneme)
-
-    # Labelクラスオブジェクト化
-    label = Label()
-    label.values = phonemes
+            monolabel = Phoneme()
+            monolabel.start = int(v[0])
+            monolabel.end = int(v[1])
+            monolabel.symbol = v[2]
+            label.append(monolabel)
+    else:
+        raise ValueError(
+            'function argument "time_unit" must be in ["100ns" (recommended), "s"]')
     return label
 
 
-class Label:
+class Label(list):
     """
     歌唱ラベルLABファイルを想定したクラス(2019/04/19から)
     """
 
-    def __init__(self):
-        """二次元リスト [[開始時刻, 終了時刻, 発音], [], ...]"""
-        self.__phonemes = []
-
-    @property
-    def values(self):
-        """propertyはgetterも兼ねるらしい"""
-        return self.__phonemes
-
-    @values.setter
-    def values(self, phonemes):
-        """
-        値を登録
-        """
-        if not isinstance(phonemes, list):
-            raise TypeError(
-                'argument \'phonemes\' must be list instance (values.setter in utaupy.label.py)')
-        self.__phonemes = phonemes
+    def __str__(self):
+        """文字列として扱うときのフォーマット"""
+        label_as_str = '\n'.join(str(monolabel) for monolabel in self)
+        return label_as_str
 
     def check_invalid_time(self, threshold=0):
         """
@@ -104,28 +91,23 @@ class Label:
         threshold: 許容される最小の発声時間(ms)
         """
         threshold_100ns = int(threshold * (10**4))
-        for phoneme in self.__phonemes:
-            duration = phoneme.end - phoneme.start
+        for monolabel in self:
+            duration = monolabel.end - monolabel.start
             if duration < threshold_100ns:
-                start = phoneme.start
-                end = phoneme.end
-                s_now = phoneme.symbol
                 print(
-                    f'    [ERROR] 発声時間が {threshold_100ns}ms 未満か負です : {phoneme}')
+                    f'    [ERROR] 発声時間が {threshold}ms 未満か負です : {monolabel}')
 
     def write(self, path_out, mode='w',
               encoding='utf-8', newline='\n', delimiter=' ', kiritan=False):
         """
         LABファイルを書き出し
         """
-        # 出力用の文字列
-        phonemes = self.__phonemes
         if kiritan:
             lines = ['{:.7f} {:.7f} {}'.format(ph.start, ph.end, ph.symbol)
-                     for ph in phonemes]  # 100ns -> 1s 表記変換
+                     for ph in self]  # 100ns -> 1s 表記変換
         else:
             lines = ['{0}{3}{1}{3}{2}'.format(
-                ph.start, ph.end, ph.symbol, delimiter) for ph in phonemes]
+                ph.start, ph.end, ph.symbol, delimiter) for ph in self]
         # ファイル出力
         with open(path_out, mode=mode, encoding=encoding, newline=newline) as f:
             f.write('\n'.join(lines))
@@ -143,18 +125,7 @@ class Phoneme:
         self.symbol = None  # 発音記号
 
     def __str__(self):
-        return f'{self.start} {self.end} {self.symbol} \t<utaupy.label.Phoneme class object>'
-
-    @property
-    def values(self):
-        """
-        値を確認する用に一応実装
-        setterは用意しない
-        """
-        d = {'self.start': self.start,
-             'self.end': self.end,
-             'self.symbol': self.symbol}
-        return d
+        return f'{self.start} {self.end} {self.symbol}'
 
 
 if __name__ == '__main__':
