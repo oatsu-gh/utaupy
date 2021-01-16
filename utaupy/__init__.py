bb27990da99902a70755a1c1c3dda42bdf867dba
oatsu-gh <panchi.psp@gmail.com>
2021-01-16 16:06:10 +0900

    utils.hts2json() を追加
    フルラベルファイルをJSONファイルに変換します。

--- utaupy/__init__.py
+++ utaupy/__init__.py
@@ -1,2 +1,2 @@
 from utaupy import (backup, convert, hts, label, otoini, reaper, reclist,
-                    shiro, svp, table, ust, utau, utauplugin)
+                    shiro, svp, table, ust, utau, utauplugin, utils)
---
+++ utaupy/utils/__init__.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+# Copyright (c) 2020 oatsu
+from ._hts2json import hts2json
