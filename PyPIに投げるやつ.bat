fb9f04c1ff056e591de44e421db75e830d0dc4db
oatsu-gh <panchi.psp@gmail.com>
2021-03-05 02:39:54 +0900

    Update PyPIに投げるやつ.bat
    ドキュメント更新のステップを追加

--- PyPIに投げるやつ.bat
+++ PyPIに投げるやつ.bat
@@ -1,3 +1,9 @@
+@rem 次の1行は初回のドキュメント生成コマンド
+@rem sphinx-apidoc -Ff -o docs/ utaupy/
+cd docs
+make html
+
+@PyPIにアップロード
 rd /s /q dist build utaupy.egg-info
 python setup.py sdist bdist_wheel
 twine check dist/*
