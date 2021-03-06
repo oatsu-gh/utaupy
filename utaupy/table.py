7dd316a5e0be17444629eaf8e4e6cb7565e49cf5
oatsu-gh <panchi.psp@gmail.com>
2021-03-06 20:20:29 +0900

    table.load() で .confファイルを読み取る機能を追加

--- utaupy/table.py
+++ utaupy/table.py
@@ -10,24 +10,60 @@ def main():
     print('平仮名とアルファベットの対応表を扱うモジュールです。')
 
 
-def load(path, encoding='utf-8'):
+def load(path, encoding='utf-8') -> dict:
+    """テーブルを読み取ってインスタンス生成"""
+    path = str(path).strip('"')
+    if path.endswith('.table'):
+        return load_table_file(path, encoding=encoding)
+    if path.endswith('.conf'):
+        return load_conf_file(path, encoding=encoding)
+    raise ValueError("Input path must end with '.table' or '.conf'.")
+
+
+def load_table_file(path_table, encoding='utf-8') -> dict:
     """テーブルを読み取ってインスタンス生成"""
     # ファイル読み取り
     try:
-        with open(path, mode='r', encoding=encoding) as f:
-            l = [v.strip().split() for v in f.readlines()]
+        with open(path_table, mode='r', encoding=encoding) as f:
+            lines = [line.strip() for line in f.readlines()]
     except UnicodeDecodeError:
         try:
-            with open(path, mode='r', encoding='sjis') as f:
-                l = [v.strip().split() for v in f.readlines()]
+            with open(path_table, mode='r', encoding='sjis') as f:
+                lines = [line.strip() for line in f.readlines()]
         except UnicodeDecodeError:
-            with open(path, mode='r', encoding='utf-8') as f:
-                l = [v.strip().split() for v in f.readlines()]
+            with open(path_table, mode='r', encoding='utf-8') as f:
+                lines = [line.strip() for line in f.readlines()]
     # 辞書にする
-    d = {}
-    for v in l:
-        d[v[0]] = v[1:]
-    return d
+    d_table = {}
+    for line in lines:
+        line_split = line.split()
+        d_table[line_split[0]] = line_split[1:]
+    return d_table
+
+
+def load_conf_file(path_conf, encoding='utf-8') -> dict:
+    """音素分類用のファイルを読み取って辞書を返す"""
+    # ファイル読み取り
+    try:
+        with open(path_conf, mode='r', encoding=encoding) as f:
+            lines = [line.strip() for line in f.readlines()]
+    except UnicodeDecodeError:
+        try:
+            with open(path_conf, mode='r', encoding='sjis') as f:
+                lines = [line.strip() for line in f.readlines()]
+        except UnicodeDecodeError:
+            with open(path_conf, mode='r', encoding='utf-8') as f:
+                lines = [line.strip() for line in f.readlines()]
+    # 辞書にする
+    d_conf = {'SILENCES': ['sil'], 'PAUSES': ['pau'], 'BREAKS': ['br']}
+    for line in lines:
+        key, value = line.split('=', 1)
+        phonemes = value.strip('"').split(',')
+        d_conf[key] = phonemes
+    if 'PHONEME_CL' in d_conf:
+        d_conf['BREAKS'] += d_conf['PHONEME_CL']
+
+    return d_conf
 
 
 if __name__ == '__main__':
