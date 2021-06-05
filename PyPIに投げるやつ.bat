c68d0b820f485f2ca121b6d319f75b28fa9f26b4
oatsu <panchi.psp@gmail.com>
2021-06-05 17:51:54 +0900

    ドキュメントを更新

--- PyPIに投げるやつ.bat
+++ PyPIに投げるやつ.bat
@@ -1,9 +1,6 @@
-@rem 次の1行は初回のドキュメント生成コマンド
-@rem sphinx-apidoc -Ff -o docs/ utaupy/
-cd docs
-make html
 
-@PyPIにアップロード
+
+@rem PyPIにアップロード
 rd /s /q dist build utaupy.egg-info
 python setup.py sdist bdist_wheel
 twine check dist/*
