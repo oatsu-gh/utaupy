74b597147b6a99a12d2e6bf2adad8d34f90883ef
oatsu <panchi.psp@gmail.com>
2021-06-27 01:48:35 +0900

    書類整理

--- PyPIに投げるやつ.bat
+++ PyPIに投げるやつ.bat
@@ -1,8 +1,9 @@
+@rem PyPIにアップロードするやつ
 
-
-@rem PyPIにアップロード
 rd /s /q dist build utaupy.egg-info
 python setup.py sdist bdist_wheel
 twine check dist/*
 REM twine upload --repository testpypi dist/* --verbose
 twine upload --repository pypi dist/* --verbose
+
+PAUSE
