25cb7f4d5868c48d0277c6d48a18bd683a17d17b
oatsu <panchi.psp@gmail.com>
2021-08-09 20:23:33 +0900

    Update utau.py
    set_by_key の部分をUserDictとして普通に登録するようにした。

--- utaupy/utau.py
+++ utaupy/utau.py
@@ -12,7 +12,7 @@ from os.path import dirname, expandvars
 from . import otoini as _otoini
 
 # Windowsのとき
-if platform.system == 'Windows':
+if str(platform.system) == 'Windows':
     import winreg
 
     def utau_root() -> str:
@@ -106,93 +106,90 @@ class UtauVoiceBank():
         super().__init__()
         self.path = path
         self.prefixmap = PrefixMap(f'{self.path}/prefix.map')
-        self.otoini = {}
+        self.otoini = _otoini.OtoIni()
         # 原音設定ファイルを取得
         all_otoini_paths = glob(f'{path}/**/oto.ini', recursive=True)
         for path_otoini in all_otoini_paths:
-            each_otoini = _otoini.load(path_otoini)
-            self.otoini.update(each_otoini.as_dict())
+            self.otoini.data += _otoini.load(path_otoini).data
 
 
-def autoselect_alias(voicebank, utaupy_ust_note):
-    """
-    voicebank       : utaupy.utau.UtauVoiceBank オブジェクト
-    utaupy_ust_note : utaupy.Ust.Note オブジェクト
-    use_atalias     : Noteオブジェクトの '@alias' を使用するかどうか。
-                      voicebankで指定しているUTAU音源が 対象USTのUTAU音源とは違う場合、
-                      Falseにするほうがよい。
 
-    Noteオブジェクトの歌詞と音程から、使用する原音に対応するエイリアスを決定する。
-    ノート情報に @alias があればそのエイリアスを使用し、
-    無い場合は prefix.map を使って決定する。
-    """
-    lyric = utaupy_ust_note.lyric
-    if lyric == 'R':
-        return 'R'
-    # USTでエイリアスを強制指定している場合
-    if lyric.startswith('?'):
-        return lyric.lstrip('?')
-    # 普通の歌詞の場合はprefixmapを参照してサフィックス追加
-    alias = lyric + voicebank.prefixmap[str(utaupy_ust_note.notenum)]
-    return alias
+    def autoselect_alias(self, utaupy_ust_note):
+        """
+        voicebank       : utaupy.utau.UtauVoiceBank オブジェクト
+        utaupy_ust_note : utaupy.Ust.Note オブジェクト
+        use_atalias     : Noteオブジェクトの '@alias' を使用するかどうか。
+                          voicebankで指定しているUTAU音源が 対象USTのUTAU音源とは違う場合、
+                          Falseにするほうがよい。
 
-
-def get_oto(voicebank, utaupy_ust_note, suffix_exists=False):
-    """
-    voicebank       : utaupy.utau.UtauVoiceBank オブジェクト
-    utaupy_ust_note : utaupy.Ust.Note オブジェクト
-    use_atparam     : Noteオブジェクトの '@alias' などを使用するかどうか。
-                      voicebankで指定しているUTAU音源が 対象USTのUTAU音源とは違う場合、
-                      Falseにするほうがよい。
-    Noteオブジェクトに対応する原音設定の値を取得する。
-    """
-    if suffix_exists is False:
-        alias = autoselect_alias(voicebank, utaupy_ust_note)
-    else:
-        alias = utaupy_ust_note.lyric
-    # 原音にちゃんとあるかどうか
-    try:
-        oto_for_the_alias = voicebank.otoini[alias]
-    # なければ全部の数値がゼロの原音設定値を返す
-    except KeyError as e:
-        print(f'[WARN] KeyError: {e}')
-        oto_for_the_alias = _otoini.Oto()
-        oto_for_the_alias.Alias = alias
-    return oto_for_the_alias
+        Noteオブジェクトの歌詞と音程から、使用する原音に対応するエイリアスを決定する。
+        ノート情報に @alias があればそのエイリアスを使用し、
+        無い場合は prefix.map を使って決定する。
+        """
+        lyric = utaupy_ust_note.lyric
+        if lyric == 'R':
+            return 'R'
+        # USTでエイリアスを強制指定している場合
+        if lyric.startswith('?'):
+            return lyric.lstrip('?')
+        # 普通の歌詞の場合はprefixmapを参照してサフィックス追加
+        # TODO: すでにサフィックスがある場合に不具合を回避する必要がある
+        alias = lyric + self.prefixmap[str(utaupy_ust_note.notenum)]
+        return alias
 
 
-def autoadjust_parameters(voicebank, utaupy_ust_ust):
-    """
-    voicebank       : utaupy.utau.UtauVoiceBank オブジェクト
-    utaupy_ust_ust  : utaupy.Ust.Ust オブジェクト
-
-    # TODO: プラグイン用に'@'パラメータに対応させる。
-    UTAUの「パラメータ自動調整」機能に相当するメソッド。
-    ・エイリアス(@alias)
-    ・STP(StartPoint)
-    ・オーバーラップ(VoiceOverlap)
-    ・先行発声(PreUtterance)
-    の4つの値を調整する。
-
-    # TODO: 子音速度100以外に対応させる。
-    仕様
-    前のノートが休符な時は、原音値をそのまま入力する。
-    前のノートが休符でない場合
-        前のノートの長さの半分が、oto.overlapからoto.preutteranceまでの長さよりも短い場合
-            差分をSTPで削る
-    """
-    notes = utaupy_ust_ust.notes
-    for i, note in enumerate(notes[1:], 1):
-        halflen = notes[i - 1].length_ms / 2
-        oto = get_oto(voicebank, note, suffix_exists=True)
-        if halflen < oto.preutterance - oto.overlap:
-            at_preuttr = halflen * oto.preutterance / (oto.preutterance - oto.overlap)
-            at_overlap = halflen * oto.overlap / (oto.preutterance - oto.overlap)
-            note.set_by_key('StartPoint', oto.preutterance - at_preuttr)
-            note.set_by_key('PreUtterance', at_preuttr)
-            note.set_by_key('VoiceOverlap', at_overlap)
-            print('adjust')
+    def get_oto(self, utaupy_ust_note, suffix_exists=False):
+        """
+        voicebank       : utaupy.utau.UtauVoiceBank オブジェクト
+        utaupy_ust_note : utaupy.Ust.Note オブジェクト
+        use_atparam     : Noteオブジェクトの '@alias' などを使用するかどうか。
+                          voicebankで指定しているUTAU音源が 対象USTのUTAU音源とは違う場合、
+                          Falseにするほうがよい。
+        Noteオブジェクトに対応する原音設定の値を取得する。
+        """
+        if suffix_exists is False:
+            alias = self.autoselect_alias(utaupy_ust_note)
+        # 原音にちゃんとあるかどうか
+        if alias in self.otoini:
+            oto_for_the_alias = self.otoini[alias]
+        # なければ全部の数値がゼロの原音設定値を返す
         else:
-            note.set_by_key('StartPoint', 0)
-            note.set_by_key('PreUtterance', oto.preutterance)
-            note.set_by_key('VoiceOverlap', oto.overlap)
+            oto_for_the_alias = _otoini.Oto()
+            oto_for_the_alias.alias = alias
+        return oto_for_the_alias
+
+
+    def autoadjust_parameters(self, utaupy_ust_ust):
+        """
+        voicebank       : utaupy.utau.UtauVoiceBank オブジェクト
+        utaupy_ust_ust  : utaupy.Ust.Ust オブジェクト
+
+        # TODO: プラグイン用に'@'パラメータに対応させる。
+        UTAUの「パラメータ自動調整」機能に相当するメソッド。
+        ・エイリアス(@alias)
+        ・STP(StartPoint)
+        ・オーバーラップ(VoiceOverlap)
+        ・先行発声(PreUtterance)
+        の4つの値を調整する。
+
+        # TODO: 子音速度100以外に対応させる。
+        仕様
+        前のノートが休符な時は、原音値をそのまま入力する。
+        前のノートが休符でない場合
+            前のノートの長さの半分が、oto.overlapからoto.preutteranceまでの長さよりも短い場合
+                差分をSTPで削る
+        """
+        notes = utaupy_ust_ust.notes
+        for i, note in enumerate(notes[1:], 1):
+            halflen = notes[i - 1].length_ms / 2
+            oto = self.get_oto(note, suffix_exists=True)
+            if halflen < oto.preutterance - oto.overlap:
+                at_preuttr = halflen * oto.preutterance / (oto.preutterance - oto.overlap)
+                at_overlap = halflen * oto.overlap / (oto.preutterance - oto.overlap)
+                note['StartPoint'] = oto.preutterance - at_preuttr
+                note['PreUtterance'] = at_preuttr
+                note['VoiceOverlap'] = at_overlap
+            else:
+                note['StartPoint'] = 0
+                note['PreUtterance'] = oto.preutterance
+                note['VoiceOverlap'] = oto.overlap
