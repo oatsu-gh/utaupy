2b151b40fae010efb2d0f817b489290f0ee999f1
oatsu <panchi.psp@gmail.com>
2021-12-28 00:34:31 +0900

    Update __init__.py

--- utaupy/utils/__init__.py
+++ utaupy/utils/__init__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-# Copyright (c) 2020 oatsu
+# Copyright (c) 2020-2021 oatsu
 from ._hts2json import hts2json
 from ._hts2ust import hts2ust
 from ._ust2hts import ust2hts, ustobj2songobj
+from ._hts2csv import hts2csv
