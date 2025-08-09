ab8320ea10675789218af6926baf945294c0c0e6
oatsu <oatsu.dev@gmail.com>
2025-08-10 03:01:56 +0900

    Update PyPIに投げるやつ.bat

--- PyPIに投げるやつ.bat
+++ PyPIに投げるやつ.bat
@@ -1,7 +1,7 @@
 @rem PyPIにアップロードするやつ
 
 rd /s /q dist build utaupy.egg-info
-python setup.py sdist bdist_wheel
+python -m build
 twine check dist/*
 REM twine upload --repository testpypi dist/* --verbose
 twine upload --repository pypi dist/* --verbose
